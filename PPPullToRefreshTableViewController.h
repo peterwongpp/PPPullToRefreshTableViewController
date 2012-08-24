6ac1c01a00cc6a07096424ba99d545b01b8cd082
PeterWong <peter@peterwongpp.com>
2012-08-24 12:52:32 +0800

    update usage

--- PPPullToRefreshTableViewController.h
+++ PPPullToRefreshTableViewController.h
@@ -8,21 +8,21 @@
 //  Usage:
 //
 //  1. Copy the files into your project:
-//      - PPPullToRefreshTableViewController.h
-//      - PPPullToRefreshTableViewController.m
-//      - pullToRefreshArrow.png
-//      - pullToRefreshArrow@2x.png
+//      - *PPPullToRefreshTableViewController.h*
+//      - *PPPullToRefreshTableViewController.m*
+//      - *pullToRefreshArrow.png*
+//      - *pullToRefreshArrow@2x.png*
 //
-//  2. your table view controller should inherit from PPPullToRefreshTableViewController (which is a UITableViewController subclass), instead of the original UITableViewController
-//      1. in YourTableViewController.h
+//  2. your table view controller should inherit from *PPPullToRefreshTableViewController* (which is a *UITableViewController* subclass), instead of the original *UITableViewController*
+//      1. in *YourTableViewController.h*
 //          1. #import "PPPullToRefreshTableViewController.h"
 //          2. @interface YourTableViewController : PPPullToRefreshTableViewController
 //      2. override the following method:
-//          1. - (void) refreshing
+//          1. *- (void) refreshing*
 //              - you should call *didRefresh* inside this method in order to hide the refresh header view
-//      3. you may want to call *willRefresh* to start refreshing in code (this method by default shows the refresh header view, and calls *refreshing* automatically).
+//      3. you may want to call *willRefresh* to start refreshing in code (this method by default shows the refresh header view, and calls *refreshing* automatically)
 //
-//  3. In your build target's **Link Binbary with Libraries** section, add the QuartzCore.framework
+//  3. In your build target's **Link Binbary with Libraries** section, add the *QuartzCore.framework*
 
 #import <UIKit/UIKit.h>
 
